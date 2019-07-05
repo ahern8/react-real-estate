e0365c7a61a36201e9a8dd6561c2b8522ff7e82f
Alejandro R. Hernandez <arhernandez@lmds-arher.jomax.paholdings.com>
2019-07-05 11:54:23 -0700

    initial commit

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,79 @@
+var listingsData = [
+  {
+    address: '20-34 Grand Ave',
+    city: 'Ridgewood',
+    state: 'NY',
+    rooms: 1,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Studio',
+    image:
+    'https://rentpath-res.cloudinary.com/w_370,h_370,t_rp,cs_tinysrgb,fl_force_strip,c_fill/e_unsharp_mask:50,q_auto/3e551a5c0debe557c97e6ed46c5eb54f'
+  },
+  {
+    address: '556 Market Ave',
+    city: 'Las Vegas',
+    state: 'NV',
+    rooms: 3,
+    price: 320000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo',
+    image:
+    'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/640x480/56587_11491_001.jpg'
+  },
+  {
+    address: '189 Main Ave',
+    city: 'Orlando',
+    state: 'FL',
+    rooms: 3,
+    price: 100000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment',
+    image:
+    'https://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4011-1/moda-apartments-exterior.jpg'
+  },
+  {
+    address: '567 Bridgewood Ave',
+    city: 'Los Angeles',
+    state: 'CA',
+    rooms: 3,
+    price: 120000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'pool'
+    ],
+    homeType: 'Condo',
+    image:
+    'https://ar.rdcpix.com/689959298/07c3a4cebbda67d6a57fa7ca2129bbbcc-f0xd-w480_h480_q80.jpg'
+  },
+  {
+    address: '456 Emerald Ave',
+    city: 'Phoenix',
+    state: 'AZ',
+    rooms: 2,
+    price: 500000,
+    floorSpace: 2500,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'House',
+    image:
+    'https://aro.nyc/wp-content/uploads/2017/11/Midtown-West-Luxury-Rentals-Living.jpg'
+  }
+]
+
+export default listingsData;
